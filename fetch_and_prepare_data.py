f65dc8188bcb77bce827d7f1d02349700099ae95
DESKTOP-NBU5D9L\NadePC <dochioiu.alex@gmail.com>
2018-10-02 20:10:50 +0300

    Some progress in parsing data from match

--- fetch_and_prepare_data.py
+++ fetch_and_prepare_data.py
@@ -1,5 +1,55 @@
 # -*- coding: utf-8 -*-
 
+def get_single_match_stats(api_token: str, server: str, match_id: str):
+    '''
+    For a given match id on a given server, we pull the following details:
+        top1_user_id, top1_champ_id, jgl1_user_id, jgl1_champ_id, mid1_user_id, 
+        mid1_champ_id, adc1_user_id, adc1_champ_id, sup1_user_id, sup1_champ_id,
+        top2_user_id, top2_champ_id, jgl2_user_id, jgl2_champ_id, mid2_user_id, 
+        mid2_champ_id, adc2_user_id, adc2_champ_id, sup2_user_id, sup2_champ_id,
+        red_team_won (boolean)
+        
+    P.S. red team is team1 ; blue team is team2
+    '''
+    
+    import urllib.request
+    import json
+    
+    contents = urllib.request.urlopen(urllib.request.Request(
+            'https://{0}.api.riotgames.com/lol/match/v3/matches/{1}'.format(server, match_id),
+            headers={"X-Riot-Token": api_token, "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",}
+            )).read().decode("utf-8")
+    
+    parsed_contents = json.loads(contents)
+    
+    returned_game_type: str = parsed_contents['gameMode'] # CLASSIC MEANS NORMAL PVP (both 5v5 and 3v3)
+    returned_map_id: int = parsed_contents['mapId'] # mapId 11 is summoner's rift (so 5v5)
+    if returned_game_type != 'CLASSIC' and returned_map_id != 11:
+        # THIS IS NOT a draft or ranked game
+        return []
+    
+    return_value = [0]*11
+    
+    def get_players_data():
+        players_data = []
+        for index in range(10):
+            players_data.append([]) # each row will contain: player_id, player_position, champ_id
+            player_rito_index: int = parsed_contents['participantIdentities'][index]['participantId']
+            if player_rito_index != index + 1:
+                print("WARNING: POSITION INDEX DOES NOT MATCH WITH RIOT PLAYER INDEX for game {0} on server {1}".format(match_id, server))
+                
+            players_data[index].append( parsed_contents['participantIdentities'][index]['player']['accountId'] )
+            players_data[index].append( parsed_contents['participants'][index]['timeline']['lane'] ) 
+            players_data[index].append( parsed_contents['participants'][index]['championId'] ) 
+            
+        return players_data
+        
+    
+    red_team_won: bool = parsed_contents['teams'][0]['win'] == 'Win'
+    return_value[10] = red_team_won
+    return_value[0] = get_players_data()
+    
+    return return_value
 
 def get_hundred_matches_ids_from_index(api_token: str, server:str, account_id: str, season: int = 11, index: int = 0):
     '''
@@ -49,3 +99,7 @@ dataset = dataset.drop(["Acc Name", "Team"], axis = 1)
 match_ids = get_all_matches_ids_for_season(api_key, dataset.iloc[10][1], dataset.iloc[10][2], 11)
 #match_ids = get_all_matches_ids_for_season(api_key, "euw1", "30648034", 11)
 print(match_ids[:10])
+
+print(get_single_match_stats(api_key, "euw1", "3782523216"))
+
+# KOREA MATCH ID 3359817093
